81493ffee9b64dc724867a0083af8e22dba66959
zk <1400547074@qq.com>
2021-08-14 20:11:49 +0800

    提交

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -15,7 +15,7 @@ $.ajaxPrefilter(function(options) {
         // console.log('执行了 complete 回调：')
         // console.log(res)
         // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        if (res.responseJSON.code === 1 && res.responseJSON.message === '身份认证失败！') {
             // 1. 强制清空 token
             localStorage.removeItem('token')
                 // 2. 强制跳转到登录页面
